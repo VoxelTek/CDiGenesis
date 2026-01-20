2afec7e96a2c8bcacab779ba0b5a9db16152e6f9
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2026-01-21 01:50:23 +1000

    Largely complete, work required for 6-button
    In the current state, the ATtiny may be polling the controller so fast that a 6-button controller would think it needs to present its additional buttons. Either this could be prevented by adding delays to prevent over-polling, or code could be reworked to use extra 3 buttons as a speed control of sorts.

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -2,7 +2,10 @@
 #include "console.h"
 
 uint8_t buttonState = 0x00;
-const int8_t speed = 0x40; // Needs to be less than 128 (0x7F or less)
+uint8_t oldButtonState = 0x00;
+int8_t speeds[6] = {0x7f, 0x60, 0x40, 0x30, 0x20, 0x10};
+uint8_t speedIndex = 0;
+bool speedChanged = false;
 const uint8_t mode = MANEUVER;
 
 int main() {
@@ -20,32 +23,43 @@ int main() {
 void loop() {
     refreshButtonState();
 
+    if ((buttonState = oldButtonState) && !(buttonState & 0b00101110)) { // If no buttons have changed, AND no D-Pad movement, return.
+        _delay_ms(20);
+        return;
+    }
+
+    oldButtonState = buttonState;
+
     uint8_t b0 = 0b1000000;
 	uint8_t b1 = 0b0000000;
 	uint8_t b2 = 0b0000000;
     int8_t x = 0;
     int8_t y = 0;
     
-    if (buttonState & 1) { // D-Pad up
-        y = speed;
+    if (buttonState & 4) { // D-Pad up
+        y = -speeds[speedIndex];
     }
-    else if (buttonState & 4) { // D-Pad down
-        y = -speed;
+    else if (buttonState & 16) { // D-Pad down
+        y = speeds[speedIndex];
     }
-    
-    if (buttonState & 2) { // Button B
+
+    if (buttonState & 32) { // D-Pad left
+        x = -speeds[speedIndex];
+    }
+    else if (buttonState & 64) { // D-Pad right
+        x = speeds[speedIndex];
+    }
+
+    if (buttonState & 2) { // Button A, btn 1
         b0 |= 0b100000;
     }
 
-    if (buttonState & 8) { // D-Pad left
-        x = speed;
-    }
-    else if (buttonState & 16) { // D-Pad right
-        x = -speed;
-    }
-
-    if (buttonState & 32) { // Button C
+    if (buttonState & 8) { // Button B, btn 2
         b0 |= 0b010000;
+    }
+        
+    if (buttonState & 128) { // Button C, btn 1+2
+        b0 |= 0b110000;
     }
 
     b0 |= ((y >> 4) & 0b1100) + (x >> 6);
@@ -54,7 +68,8 @@ void loop() {
 
     printf(b0);
     printf(b1);
-    printf(b2);    
+    printf(b2);
+    _delay_ms(5);
 }
 
 void initCDi() {
@@ -65,7 +80,34 @@ void initCDi() {
 void refreshButtonState() {
     gpio_set_high(SELECT); // Disable SELECT
     _delay_ms(2);
-    buttonState = ((~PORTA.IN & 0b01111110) >> 1); // Get D-Pad, B, C
+    buttonState = (~PORTA.IN & 0b11000000) + ((~PORTA.IN & 0b0001110) << 1); // Get D-Pad, B, C
+
+    gpio_set_low(SELECT); // Enable SELECT
+    _delay_ms(2);
+    while ((PORTA.IN & 0b01010000)) { // Wait until D-Left and D-Right pressed
+        
+    }
+    buttonState += ((~PORTA.IN & 4) >> 1) + ((~PORTA.IN & 128) >> 7); // Get A, Start
+    gpio_set_high(SELECT); // Disable SELECT
+
+    if ((buttonState & 1) && !speedChanged) { // Decrease speed value
+        speedIndex = (speedIndex + 1) % 6;
+        speedChanged = true;
+    }
+    else if (!(buttonState & 1) && speedChanged) { // Prevent rapid speed cycling
+        speedChanged = false;
+    }
+
+    /*
+    Bit 1 (1)   = START
+    Bit 2 (2)   = A
+    Bit 3 (4)   = D-Up
+    Bit 4 (8)   = B
+    Bit 5 (16)  = D-Down
+    Bit 6 (32)  = D-Left
+    Bit 7 (64)  = D-Right
+    Bit 8 (128) = C
+    */
 }
 
 void initPins() {
@@ -77,8 +119,8 @@ void initPins() {
     PORTA.PIN2CTRL = PORT_PULLUPEN_bm;
     PORTA.PIN3CTRL = PORT_PULLUPEN_bm;
     PORTA.PIN4CTRL = PORT_PULLUPEN_bm;
-    PORTA.PIN5CTRL = PORT_PULLUPEN_bm;
     PORTA.PIN6CTRL = PORT_PULLUPEN_bm;
+    PORTA.PIN7CTRL = PORT_PULLUPEN_bm;
 
     gpio_input(RTS);
 
@@ -90,7 +132,8 @@ void initPins() {
     PA2 <-- /Btn B
     PA3 <-- /D-Down
     PA4 <-- /D-Left
-    PA5 <-- /D-Right
-    PA6 <-- /Btn C
+    PA5 --> /SELECT
+    PA6 <-- /D-Right
+    PA7 <-- /Btn C
     */
 }
